5cbca82281de56e36642d4d3316b815001b7d1d4
Chris <ccuddohy@gmail.com>
2020-09-02 14:25:06 -0400

    modified push bat file

--- XertClient/local_nuget_push.bat
+++ XertClient/local_nuget_push.bat
@@ -1,1 +1,1 @@
-dotnet nuget push "bin\release\*.nupkg" -s C:\Users\ccudd\source\repos\MyNugetRepo
+dotnet nuget push "bin\Release\netcoreapp3.1\publish\*.nupkg" -s C:\Users\ccudd\source\repos\MyNugetRepo
